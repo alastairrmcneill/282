4863fbcd2be31c21dca6fe8edb32c687a57bd23d
Alastair McNeill <alastair.r.mcneill@gmail.com>
2024-04-27 12:06:34 +0100

    7.1.03 - Add date to bulk update

--- lib/screens/bulk_munro_updates/bulk_munro_update_screen.dart
+++ lib/screens/bulk_munro_updates/bulk_munro_update_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
@@ -34,10 +35,60 @@ class BulkMunroUpdateScreen extends StatelessWidget {
           Map<String, dynamic> e =
               bulkMunroUpdateState.bulkMunroUpdateList.firstWhere((element) => element[MunroFields.id] == munro.id);
 
+          List<dynamic> summitedDates = e[MunroFields.summitedDates] as List<dynamic>;
+
+          var firstSummitedDate = summitedDates.isEmpty ? null : summitedDates[0];
+          if (firstSummitedDate != null) {
+            if (firstSummitedDate is Timestamp) {
+              firstSummitedDate = (firstSummitedDate as Timestamp).toDate();
+            }
+          } else {
+            firstSummitedDate = DateTime.now();
+          }
+
+          TextEditingController _dateController =
+              TextEditingController(text: DateFormat('dd/MM/yyyy').format(firstSummitedDate));
+
+          DateTime? _pickedStartDate;
           return ListTile(
             title: Text(munro.name),
             subtitle: munro.extra != null ? Text(munro.extra!) : null,
             leading: e[MunroFields.summited] ? const Icon(Icons.check) : const SizedBox(),
+            trailing: e[MunroFields.summited]
+                ? SizedBox(
+                    width: 100,
+                    child: TextFormField(
+                      controller: _dateController,
+                      readOnly: true,
+                      onTap: () async {
+                        _pickedStartDate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(1900),
+                          lastDate: DateTime.now(),
+                        );
+
+                        if (_pickedStartDate != null) {
+                          DateTime date = _pickedStartDate!.add(const Duration(hours: 12));
+                          if (munro.summitedDates!.isEmpty) {
+                            munro.summitedDates!.add(date);
+                          } else {
+                            munro.summitedDates![0] = date;
+                          }
+                          bulkMunroUpdateState.setMunro = {
+                            MunroFields.id: munro.id,
+                            MunroFields.summitedDates: munro.summitedDates,
+                          };
+                        }
+                      },
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Required';
+                        }
+                      },
+                    ),
+                  )
+                : null,
             onTap: () {
               if (e[MunroFields.summited]) {
                 bulkMunroUpdateState.setMunro = {
@@ -47,11 +98,13 @@ class BulkMunroUpdateScreen extends StatelessWidget {
                   MunroFields.summitedDates: []
                 };
               } else {
+                DateTime now = DateTime.now();
+                DateTime today = DateTime(now.year, now.month, now.day, 12, 0, 0, 0, 0);
                 bulkMunroUpdateState.setMunro = {
                   MunroFields.id: munro.id,
                   MunroFields.summited: !e[MunroFields.summited],
-                  MunroFields.summitedDate: DateTime.now(),
-                  MunroFields.summitedDates: munro.summitedDates!.isEmpty ? [DateTime.now()] : munro.summitedDates!
+                  MunroFields.summitedDate: today,
+                  MunroFields.summitedDates: munro.summitedDates!.isEmpty ? [today] : munro.summitedDates!
                 };
               }
             },
