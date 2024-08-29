2b8ec6932fcf7751491576e900b1c75709d39ff7
Alastair McNeill <alastair.r.mcneill@gmail.com>
2024-08-29 07:28:32 +0100

    Bug fix for ratings not working

--- lib/screens/explore/state/munro_state.dart
+++ lib/screens/explore/state/munro_state.dart
@@ -46,8 +46,8 @@ class MunroState extends ChangeNotifier {
 
   set setMunroList(List<Munro> munroList) {
     _munroList = munroList;
-    _filter();
     notifyListeners();
+    _filter();
   }
 
   set setSelectedMunro(Munro? selectedMunro) {
