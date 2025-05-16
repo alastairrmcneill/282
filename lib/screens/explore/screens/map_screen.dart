ea95aabdad9b05b8e1baecbe0a3617bbae7c896c
Alastair McNeill <alastair.r.mcneill@gmail.com>
2025-05-16 15:50:39 +0100

    got new icons slowly working

--- lib/screens/explore/screens/map_screen.dart
+++ lib/screens/explore/screens/map_screen.dart
@@ -25,20 +25,23 @@ class _MapScreenState extends State<MapScreen> {
   bool loading = true;
   late MapboxMap _mapboxMap;
   late PointAnnotationManager _annotationManager;
+  PointAnnotation? _previousSelectedAnnotation;
+  PointAnnotation? _selectedAnnotation;
   List<PointAnnotation?> currentAnnotations = [];
   final Map<String, PointAnnotation> _annotationMap = {};
 
-  double _zoom = 5.5;
   String? _selectedMunroID;
   bool showTerrain = false;
   bool _tappedAnnotation = false;
 
-  CameraOptions camera = CameraOptions(
-    center: Point(coordinates: Position(-98.0, 39.5)),
-    zoom: 2,
-    bearing: 0,
-    pitch: 0,
+  CameraOptions startingCamera = CameraOptions(
+    center: Point(coordinates: Position(-4.2, 56.8)),
+    zoom: 5.5,
   );
+
+  late Uint8List incompleteIcon;
+  late Uint8List completeIcon;
+  late Uint8List selectedIcon;
 
   @override
   void initState() {
@@ -48,6 +51,10 @@ class _MapScreenState extends State<MapScreen> {
   }
 
   void loadData() async {
+    incompleteIcon = await _loadMarker('assets/munro_incomplete.png');
+    completeIcon = await _loadMarker('assets/munro_complete.png');
+    selectedIcon = await _loadMarker('assets/munro_selected.png');
+
     await MunroDatabase.loadBasicMunroData(context).then(
       (value) => setState(() => loading = false),
     );
@@ -57,14 +64,12 @@ class _MapScreenState extends State<MapScreen> {
     _mapboxMap = mapboxMap;
     await mapboxMap.compass.updateSettings(CompassSettings(enabled: false));
     await mapboxMap.scaleBar.updateSettings(ScaleBarSettings(enabled: false));
-
     _mapboxMap.setBounds(
       CameraBoundsOptions(
         bounds: CoordinateBounds(
           southwest: Point(
             coordinates: Position(-10, 53),
-          ), // too north, too south, too zoomed out, not enough zoomed in
-
+          ),
           northeast: Point(
             coordinates: Position(0, 62),
           ),
@@ -80,28 +85,22 @@ class _MapScreenState extends State<MapScreen> {
   Future<void> _addMunroSymbols({required MunroState munroState}) async {
     final List<Munro> munros = munroState.filteredMunroList;
 
-    final Uint8List incomplete = await _loadMarker('assets/munro_incomplete.png');
-    final Uint8List complete = await _loadMarker('assets/munro_complete.png');
-    final Uint8List selected = await _loadMarker('assets/munro_selected.png');
-
     _annotationManager = await _mapboxMap.annotations.createPointAnnotationManager();
 
-    // 👇 Attach tap listener
     List<PointAnnotationOptions> markers = [];
-    double iconSize = getIconSize();
 
     for (var munro in munros) {
       final icon = _selectedMunroID == munro.id
-          ? selected
+          ? selectedIcon
           : munro.summited
-              ? complete
-              : incomplete;
+              ? completeIcon
+              : incompleteIcon;
 
       markers.add(
         PointAnnotationOptions(
           geometry: Point(coordinates: Position(munro.lng, munro.lat)),
           image: icon,
-          iconSize: 0.6, //iconSize,
+          iconSize: 0.6,
         ),
       );
     }
@@ -114,39 +113,57 @@ class _MapScreenState extends State<MapScreen> {
 
     _annotationManager.addOnPointAnnotationClickListener(
       MunroAnnotationClickListener(
-        currentAnnotations: currentAnnotations,
-        munros: munros,
-        onMunroSelected: (id) async {
-          print("🚀 ~ _MapScreenState ~ void _onMunroSelected ~ id: $id");
-          setState(() {
-            _selectedMunroID = id;
-          });
-          munroState.setSelectedMunroId = id;
-          await _refreshAnnotations(munroState);
-        },
-        onAnnotationTap: () {
+        onMunroAnnotationSelected: (annotation) async {
           _tappedAnnotation = true;
+          if (annotation.id == _selectedAnnotation?.id) {
+            return true;
+          }
+
+          if (_previousSelectedAnnotation != null && _selectedAnnotation != null) {
+            // Delete the previously selected annotation
+            _annotationManager.delete(_selectedAnnotation!);
+
+            // Create a new annotation with the original icon
+            var newPreviouslySelectedAnnotation = PointAnnotationOptions(
+              geometry: _previousSelectedAnnotation!.geometry,
+              image: _previousSelectedAnnotation!.image,
+              iconSize: 0.6,
+            );
+
+            var recreatedAnnotation = await _annotationManager.create(newPreviouslySelectedAnnotation);
+
+            // Update the currentAnnotations list
+            int previousIndex = currentAnnotations.indexWhere(
+              (element) => element?.id == _previousSelectedAnnotation?.id,
+            );
+            if (previousIndex != -1) {
+              currentAnnotations[previousIndex] = recreatedAnnotation;
+            }
+
+            _previousSelectedAnnotation = null;
+            _selectedAnnotation = null;
+          }
+
+          // Delete the one we just clicked
+          _annotationManager.delete(annotation);
+
+          var newAnnotation = PointAnnotationOptions(
+            geometry: annotation.geometry,
+            image: selectedIcon,
+            iconSize: 0.6,
+          );
+
+          _previousSelectedAnnotation = annotation;
+          _selectedAnnotation = await _annotationManager.create(newAnnotation);
+
+          var index = currentAnnotations.indexWhere((element) => element?.id == annotation.id);
+          currentAnnotations[index] = _selectedAnnotation;
+
+          _selectedMunroID = munros[index].id;
+          munroState.setSelectedMunroId = munros[index].id;
         },
       ),
     );
-  }
-
-  Future<void> _refreshAnnotations(MunroState munroState) async {
-    await _annotationManager.deleteAll();
-    await _addMunroSymbols(munroState: munroState);
-  }
-
-  void _updateMarkerSizes() {
-    final newSize = getIconSize();
-
-    for (var annotation in currentAnnotations) {
-      if (annotation != null) {
-        annotation.iconSize = newSize;
-
-        print("🚀 ~ _MapScreenState ~ void_updateMarkerSizes ~ annotation: ${annotation.iconSize}");
-        _annotationManager.update(annotation);
-      }
-    }
   }
 
   Future<Uint8List> _loadMarker(String assetPath) async {
@@ -154,12 +171,6 @@ class _MapScreenState extends State<MapScreen> {
     ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(), targetWidth: 100);
     ui.FrameInfo fi = await codec.getNextFrame();
     return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!.buffer.asUint8List();
-  }
-
-  double getIconSize() {
-    const m = 0.070588;
-    const b = 0.1118;
-    return m * _zoom + b;
   }
 
   @override
@@ -180,32 +191,34 @@ class _MapScreenState extends State<MapScreen> {
                       key: const ValueKey("mapWidget"),
                       onMapCreated: (MapboxMap mapboxMap) => _onMapCreated(mapboxMap, munroState),
                       styleUri: "mapbox://styles/alastairm94/cmap1d7ho01le01s30cz9gt8v",
-                      cameraOptions: CameraOptions(
-                        center: Point(coordinates: Position(-4.2, 56.8)),
-                        zoom: _zoom,
-                      ),
+                      cameraOptions: startingCamera,
                       onTapListener: (MapContentGestureContext eventData) async {
-                        print("🚀 ~ _MapScreenState ~ void _onTapListener ~ _tappedAnnotation: $_tappedAnnotation");
+                        widget.searchFocusNode.unfocus();
+
                         if (_tappedAnnotation) {
                           _tappedAnnotation = false;
                           return;
                         }
-                        widget.searchFocusNode.unfocus();
-                        if (_selectedMunroID != null) {
-                          setState(() => _selectedMunroID = null);
-                          munroState.setSelectedMunroId = null;
-                          await _refreshAnnotations(munroState);
+
+                        setState(() => _selectedMunroID = null);
+                        munroState.setSelectedMunroId = null;
+
+                        if (_previousSelectedAnnotation != null && _selectedAnnotation != null) {
+                          // Delete the previously selected annotation
+                          _annotationManager.delete(_selectedAnnotation!);
+
+                          // Create a new annotation with the original icon
+                          var newPreviouslySelectedAnnotation = PointAnnotationOptions(
+                            geometry: _previousSelectedAnnotation!.geometry,
+                            image: _previousSelectedAnnotation!.image,
+                            iconSize: 0.6,
+                          );
+
+                          _annotationManager.create(newPreviouslySelectedAnnotation);
+                          _previousSelectedAnnotation = null;
+                          _selectedAnnotation = null;
                         }
                       },
-                      // onCameraChangeListener: (eventData) async {
-                      //   final cameraState = await _mapboxMap.getCameraState();
-                      //   final newZoom = cameraState.zoom;
-
-                      //   if ((newZoom - _zoom).abs() > 0.1) {
-                      //     _zoom = newZoom;
-                      //     // _updateMarkerSizes();
-                      //   }
-                      // },
                     ),
                     Align(
                       alignment: Alignment.bottomCenter,
@@ -220,26 +233,16 @@ class _MapScreenState extends State<MapScreen> {
 }
 
 class MunroAnnotationClickListener extends OnPointAnnotationClickListener {
-  final List<PointAnnotation?> currentAnnotations;
-  final List<Munro> munros;
-  final Function(String) onMunroSelected;
-  final VoidCallback onAnnotationTap;
+  final Function(PointAnnotation) onMunroAnnotationSelected;
 
   MunroAnnotationClickListener({
-    required this.currentAnnotations,
-    required this.munros,
-    required this.onMunroSelected,
-    required this.onAnnotationTap,
+    required this.onMunroAnnotationSelected,
   });
 
   @override
   bool onPointAnnotationClick(PointAnnotation annotation) {
-    onAnnotationTap(); // Set the flag in parent
-    final index = currentAnnotations.indexOf(annotation);
-    if (index != -1) {
-      final tappedMunro = munros[index];
-      onMunroSelected(tappedMunro.id);
-    }
+    print("Annotation clicked: ${annotation.id}");
+    onMunroAnnotationSelected(annotation);
     return true;
   }
 }
